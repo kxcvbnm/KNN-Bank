517f3d33d900c6ca55f70b72119dd86cbe2ded81
kxcvbnm <khunanont.ti@gmail.com>
2025-10-27 23:47:46 +0700

    setup and connected to mysql database

---
+++ src/main/java/com/knn/knnbank/KnnbankApplication.java
@@ -0,0 +1,13 @@
+package com.knn.knnbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnnbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnnbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knn/knnbank/KnnbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knn.knnbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnnbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
