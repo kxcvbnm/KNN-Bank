383caba0561680d1ea003c6968a417bb7bda20ad
kxcvbnm <khunanont.ti@gmail.com>
2025-10-28 18:33:26 +0700

    add response class

---
+++ src/main/java/com/knn/knnbank/response/Response.java
@@ -0,0 +1,23 @@
+package com.knn.knnbank.response;
+
+import java.io.Serializable;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Response<T> {
+    
+    private int statusCode;
+
+    private String message;
+
+    private T date;
+
+    private Map<String, Serializable> meta; 
+}
