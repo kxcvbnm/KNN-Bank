bcbcfe5b98b986350c99e72bafbbe2a1b763e71a
kxcvbnm <khunanont.ti@gmail.com>
2025-11-03 14:28:51 +0700

    add custom query to fix error

--- src/main/java/com/knn/knnbank/transaction/repo/TransactionRepo.java
+++ src/main/java/com/knn/knnbank/transaction/repo/TransactionRepo.java
@@ -5,13 +5,20 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.knn.knnbank.transaction.entity.Transaction;
 
 public interface TransactionRepo extends JpaRepository<Transaction, Long> {
     
+    @Query("SELECT t FROM Transaction t WHERE t.account.accountNumber = :accountNumber " +
+            "OR (t.transactionType = 'TRANSFER' AND t.destinationAccount = :accountNumber) " +
+            "ORDER BY t.transactionDate DESC")
     Page<Transaction> findByAccount_AccountNumber(String accountNumber, Pageable pageable);
 
+    @Query("SELECT t FROM Transaction t WHERE t.account.accountNumber = :accountNumber " +
+            "OR (t.transactionType = 'TRANSFER' AND t.destinationAccount = :accountNumber) " +
+            "ORDER BY t.transactionDate DESC")
     List<Transaction> findByAccount_AccountNumber(String accountNumber);
     
 }
