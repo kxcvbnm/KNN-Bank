0ee72c48188677104734de58626cf17646f1c637
kxcvbnm <khunanont.ti@gmail.com>
2025-11-06 01:57:57 +0700

    update main

--- src/main/java/com/knn/knnbank/KnnbankApplication.java
+++ src/main/java/com/knn/knnbank/KnnbankApplication.java
@@ -2,12 +2,30 @@ package com.knn.knnbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import lombok.RequiredArgsConstructor;
 
 @SpringBootApplication
+@EnableAsync
+@RequiredArgsConstructor
 public class KnnbankApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KnnbankApplication.class, args);
 	}
 
+	// @Bean
+	// CommandLineRunner runner() {
+	// 	return args -> {
+	// 		NotificationDTO notificationDTO = NotificationDTO.builder()
+	// 			.recipient("khunanont.ti@gmail.com")
+	// 			.subject("test email")
+	// 			.body("Hello welcome to knn bank")
+	// 			.type(NotificationType.EMAIL)
+	// 			.build();
+			
+	// 		notificationService.sendEmail(notificationDTO, new User());
+	// 	};
+	// }
 }
